2906b5d2e0262394477d631e7909fc052601d812
Lucifer472 <JanvI@0427>
2024-08-01 12:26:26 +0530

    added product page demo

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,13 @@ import ContactUsPage from "./pages/ContactUsPage";
 import AboutUsPage from "./pages/AboutUsPage";
 import PrivacyPolicyPage from "./pages/PrivacyPolicyPage";
 import TermsConditionsPage from "./pages/TermsConditionsPage";
+import ProductPage from "./pages/ProductPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route index element={<HomePage />} />
+      <Route path="/products" element={<ProductPage />} />
       <Route path="/contact-us" element={<ContactUsPage />} />
       <Route path="/about-us" element={<AboutUsPage />} />
       <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
---
+++ src/pages/ProductPage.jsx
@@ -0,0 +1,15 @@
+import { MainHeader } from "../components/general";
+
+const ProductPage = () => {
+  return (
+    <main>
+      <MainHeader
+        img={"/images/header-images/contact.png"}
+        link={"/products"}
+        title="Product Page"
+      />
+    </main>
+  );
+};
+
+export default ProductPage;
